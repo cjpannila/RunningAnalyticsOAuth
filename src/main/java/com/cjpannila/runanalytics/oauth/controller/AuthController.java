40e8230a6d3a101694136a7c3d094af6e0e2b9d9
cjpannila <chinthanapannila@gmail.com>
2026-07-23 21:57:56 +1000

    Removed unwanted imports

--- src/main/java/com/cjpannila/runanalytics/oauth/controller/AuthController.java
+++ src/main/java/com/cjpannila/runanalytics/oauth/controller/AuthController.java
@@ -6,23 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
